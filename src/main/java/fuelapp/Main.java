e1a2a0943b824d928e04a7d4416d17a2f4ae6f82
Group13-art <collensilombo8@gmail.com>
2025-08-11 16:59:00 +0200

    changed background color and icone image

--- src/main/java/fuelapp/Main.java
+++ src/main/java/fuelapp/Main.java
@@ -5,12 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.image.Image;
+
+import javax.swing.*;
+import java.util.Objects;
+
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
+
+        stage.getIcons().add(new Image(getClass().getResource("/Image.jpg").toExternalForm()));
+
+
+
+
         stage.setTitle("Fuel Administration App");
         stage.setScene(scene);
         stage.show();
